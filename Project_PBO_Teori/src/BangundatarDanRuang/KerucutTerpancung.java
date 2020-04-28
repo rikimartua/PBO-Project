d81871b4a6dece64f171d1ba3ffd237f6ea7ed63
Muh Akli Adnan <56907688+akli027@users.noreply.github.com>
2020-04-28 12:19:50 +0700

    Update KerucutTerpancung.java

--- Project_PBO_Teori/src/BangundatarDanRuang/KerucutTerpancung.java
+++ Project_PBO_Teori/src/BangundatarDanRuang/KerucutTerpancung.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BangundatarDanRuang;
 
 /**
  *
- * @author RIKI MARTUA
+ * @author HP
  */
 public class KerucutTerpancung extends Kerucut{
 
@@ -18,6 +14,9 @@ public class KerucutTerpancung extends Kerucut{
         double a = t+Math.pow(r-r1,2);
         double A = Math.sqrt(a);
         double hasilLuasKerucutPancung = Math.PI*A*(r1+r)+Math.PI*(Math.pow(r1, 2)+Math.pow(r,2));
+         if(hasilLuasKerucutPancung  == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
         return hasilLuasKerucutPancung;
     }
     
@@ -26,6 +25,9 @@ public class KerucutTerpancung extends Kerucut{
         double kerucut2 = volumeKerucut(r,t);
         double kerucut1 = volumeKerucut(r1,t1);
         double hasilVolumeKerucutPancung = kerucut2 - kerucut1;
+          if(hasilVolumeKerucutPancung  == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
         return hasilVolumeKerucutPancung;
     }
     
