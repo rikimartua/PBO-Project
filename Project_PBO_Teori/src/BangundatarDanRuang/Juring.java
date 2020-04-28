1aa0fb2bbfc2ae06d228ecaca120cdedd158b043
Muh Akli Adnan <56907688+akli027@users.noreply.github.com>
2020-04-28 12:18:12 +0700

    Update Juring.java

--- Project_PBO_Teori/src/BangundatarDanRuang/Juring.java
+++ Project_PBO_Teori/src/BangundatarDanRuang/Juring.java
@@ -15,25 +15,37 @@ public class Juring extends Lingkaran{
         super(r);
     }
     
-    public double panjangBusur(double sudut, double r){
+     public double panjangBusur(double sudut, double r){
         double bagian = sudut/360;
         double hasilBusur = bagian*2*Math.PI*r;
-        return hasilBusur;
+        if(hasilBusur == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
+         return hasilBusur;
     }
      public double kelilingJuring(double sudut, double r){
         double busur = panjangBusur(sudut,r);
         double hasilKelilingJuring =busur+2*r;
+        if(hasilKelilingJuring == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
         return hasilKelilingJuring;
     }
     public double luasJuring(double sudut, double r){
         double bagian = sudut/360;
         double hasilLuasJuring = bagian*getLuas();
+        if(hasilLuasJuring == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
         return hasilLuasJuring;
     }
     public double volumeJuring(double r, double t){
         double hasilVolumeJuring = 2*Math.PI*r*r*t;
+        if(hasilVolumeJuring == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
         return hasilVolumeJuring;
     }
     
-    
+      
 }
