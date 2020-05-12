4b8c40005c28168fe80cdf817864c38a211239d0
Willian Kelvin <48118867+WillianKelvin@users.noreply.github.com>
2020-05-12 23:01:09 +0700

    Add files via upload

---
+++ Project_PBO_Teori/src/BangundatarDanRuang/Bola.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BangundatarDanRuang;
+
+/**
+ *
+ * @author RIKI MARTUA
+ */
+public class Bola extends Lingkaran{
+
+    public Bola(double r) {
+        super(r);
+    }
+    
+     public double volumeBola(double r) {
+        double jari=Math.pow(super.r, 3);
+        double hasilVolumeBola = (4*Math.PI*jari/3);     
+         if(hasilVolumeBola  == 0){		
+			throw new ArithmeticException("Angka pembagi tidak boleh nol!");
+		}
+        return hasilVolumeBola;
+    }
+    
+}
